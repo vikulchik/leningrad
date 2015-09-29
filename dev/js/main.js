27afebe49b411f659db916c1d4693116c9afb6e9
viktoria <kvvendetta@gmail.com>
2015-09-29 13:58:12 +0300

    slider

---
+++ dev/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider();
+});
+
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+    pause: 3000,
+    slideMargin: 20
+});
