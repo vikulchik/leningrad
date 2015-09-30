126eed9fe4f65ba9ef6ba39482735c783092c04a
viktoria <kvvendetta@gmail.com>
2015-09-30 14:39:47 +0300

    lenongrad

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,6 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider();
-});
-
 $('.bxslider').bxSlider({
     auto: true,
-    autoControls: true,
     pause: 3000,
-    slideMargin: 10
+    slideMargin: 10,
+    pager: false
 });
